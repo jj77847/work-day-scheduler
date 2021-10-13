8874073d7b81133e1c313d0e01324792cf05a48d
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-10-13 19:48:19 +0100

    timer js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // timer
-$("#currentDay").text(moment().format("dddd, Do  MMMM YYYY, H:mm:ss"));
+$("#currentDay").text(moment().format("dddd, Do MMMM YYYY, H:mm:ss"));
 
 const currentTime = moment().format("H");
 console.log(currentTime);
