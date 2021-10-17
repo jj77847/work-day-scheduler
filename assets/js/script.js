809787e559cda03bc75a599412298ed880f244a7
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-10-17 14:40:19 +0100

    momentum syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,9 @@ window.onload = function () {
 };
 
 // // Below will display the current date and day for the day you use the webpage
-// This uses moment.js to give the date, everything is the brackets can be amended to whatever date format you choose.
+// This uses moment.js to give the date, the brackets can be amended to whatever date format you choose.
 
-$("#currentDay").text(moment().format("dddd, Do MMMM, YYYY kk:mm:ss"));
+$("#currentDay").text(moment().format("dddd Do MMMM YYYY kk:mm:ss"));
 
 // The function below will change the colours of the hours of the day depending on the current time
 // The below uses moment.js to record the current time in to a variable
